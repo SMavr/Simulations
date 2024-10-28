60e7918fdaa38f79c19904c1eca01612ca67af1f
SMavr <smavrikis@gmail.com>
2024-10-28 17:15:33 +0200

    Y wrap up

--- Clusters/Circle.cs
+++ Clusters/Circle.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using MonoGame.Extended;
+using System;
 
 namespace Clusters;
 
@@ -8,6 +9,9 @@ internal class Circle
 {
     public Vector2 Position { get; set; }
     public Color Color { get; set; }
+
+    private Random random = new Random(22);
+    private int radius = 2;
 
     public Circle(Vector2 position, Color color)
     {
@@ -17,6 +21,17 @@ internal class Circle
 
     public void Draw(SpriteBatch spriteBatch)
     {
-        spriteBatch.DrawCircle(Position, 2, 100, Color, 5); // 100 segments for smoothness, 2f thickness
+        spriteBatch.DrawCircle(Position, radius, 100, Color, 5); // 100 segments for smoothness, 2f thickness
+    }
+
+    public void Move(float deltaTime)
+    {
+        Position += new Vector2(random.Next(-100, 100) * deltaTime, random.Next(-100, -100) * deltaTime);
+
+        if (Position.Y + radius * 2 < 0)
+        {
+            Position = new Vector2(Position.X, 600);
+        }
+
     }
 }
--- Clusters/ClustersGame.cs
+++ Clusters/ClustersGame.cs
@@ -3,6 +3,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using MonoGame.Extended;
+using System;
 
 
 namespace Clusters;
@@ -40,6 +41,7 @@ public class ClustersGame : Game
             Exit();
 
         float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+        circle.Move(deltaTime);
 
         base.Update(gameTime);
     }
