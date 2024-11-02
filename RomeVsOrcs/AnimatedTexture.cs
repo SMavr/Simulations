f9d7340a1c044b97ea30fdff2603b4816fe508b9
SMavr <smavrikis@gmail.com>
2024-11-02 04:01:56 +0200

    seems to work the fight

--- RomeVsOrcs/AnimatedTexture.cs
+++ RomeVsOrcs/AnimatedTexture.cs
@@ -82,10 +82,12 @@ public class AnimatedTexture
 
     public void DrawFrame(SpriteBatch batch, int frame, Vector2 screenPos, int row)
     {
+        var oppositeRow = 62 - row;
+
         Rectangle sourcerect =
             row <= 51 ?
             new Rectangle(frameWidth * frame, row * 64, frameWidth, 64) :
-            new Rectangle(128 * frame, texture.Height - 128, 128, 128);
+            new Rectangle(128 * frame, texture.Height - (oppositeRow * 128), 128, 128);
        
         var prob = row <= 51 ? Origin : new Vector2(32, 32);
 
