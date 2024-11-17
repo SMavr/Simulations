8a0506ab42b3569e7339c1611b9d8efe54b694d2
SMavr <smavrikis@gmail.com>
2024-11-17 20:24:58 +0200

    added menu dialog

--- RomeVsOrcs/UIComponents/SettingsButton.cs
+++ RomeVsOrcs/UIComponents/SettingsButton.cs
@@ -5,21 +5,24 @@ using Microsoft.Xna.Framework.Input;
 using Myra.Graphics2D;
 using Myra.Graphics2D.TextureAtlases;
 using Myra.Graphics2D.UI;
+using System.ComponentModel;
 
 namespace RomeVsOrcs.UIComponents;
 internal class SettingsButton(ContentManager content, GraphicsDeviceManager graphics, Desktop desktop)
 {
-    Dialog dialog;
+    Dialog hotkeysDialog;
+    Dialog menuDialog;
 
     public void Load()
     {
         LoadButton();
-        LoadDialog();
+        LoadSettingsDialog();
+        LoadMenuDialog();
     }
 
-    private void LoadDialog()
+    private void LoadSettingsDialog()
     {
-        dialog = new Dialog
+        hotkeysDialog = new Dialog
         {
             Title = "Hot Keys",
             Padding = new Thickness(20)
@@ -35,11 +38,11 @@ internal class SettingsButton(ContentManager content, GraphicsDeviceManager grap
         AddLabel("Full Screen:","F11");
         AddLabel("Exit Game:","ESC");
 
-        dialog.Content = stackPanel;
+        hotkeysDialog.Content = stackPanel;
 
-        dialog.ButtonOk.Click += (s, e) =>
+        hotkeysDialog.ButtonOk.Click += (s, e) =>
         {
-            dialog.Close();
+            hotkeysDialog.Close();
         };
 
         void AddLabel(string textA, string textB)
@@ -84,10 +87,61 @@ internal class SettingsButton(ContentManager content, GraphicsDeviceManager grap
         // Add an event handler for the button click
         button.Click += (s, e) =>
         {
-            dialog.ShowModal(desktop);
+            menuDialog.ShowModal(desktop);
         };
 
         // Add the button to the desktop
         desktop.Widgets.Add(button);
+    }
+
+    private void LoadMenuDialog()
+    {
+        menuDialog = new Dialog
+        {
+            Title = "Menu",
+            Padding = new Thickness(20)
+        };
+
+        var stackPanel = new VerticalStackPanel
+        {
+            Spacing = 4
+        };
+
+        var titleContainer = new Panel
+        {
+        };
+
+        var titleLabel = new Label
+        {
+            Text = "Choose Option",
+            HorizontalAlignment = HorizontalAlignment.Center
+        };
+
+        titleContainer.Widgets.Add(titleLabel);
+        stackPanel.Widgets.Add(titleContainer);
+
+        var menuItem1 = new MenuItem();
+        menuItem1.Text = "Start New Game";
+        menuItem1.Selected += (s, a) =>
+        {
+            // "Start New Game" selected
+        };
+
+        var menuItem2 = new MenuItem();
+        menuItem2.Text = "Options";
+
+        var menuItem3 = new MenuItem();
+        menuItem3.Text = "Quit";
+
+        var verticalMenu = new VerticalMenu();
+
+        verticalMenu.Items.Add(menuItem1);
+        verticalMenu.Items.Add(menuItem2);
+        verticalMenu.Items.Add(menuItem3);
+
+        stackPanel.Widgets.Add(verticalMenu);
+
+        menuDialog.Content = stackPanel;
+        //desktop.Widgets.Add(stackPanel);
     }
 }
